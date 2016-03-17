f2db2674f9d9673824205fe43578f8ee153a3ed1
Evo Stamatov <aviolito@gmail.com>
2016-03-17 16:13:49 +1100

    first version

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # livereload-simple-server
+
+This is a very simple wrapper around [node-livereload](https://github.com/napcs/node-livereload) that uses [rc](https://www.npmjs.com/package/rc) to load its configuration.
+
+Refer to the above two projects for configuration, but in essence:
+
+### Defaults
+
+```
+{
+  "port": 35729,
+  "exts": ["html", "css", "js", "png", "gif", "jpg", "jpeg", "php", "php5", "py", "rb", "erb", "coffee", "htm"],
+  "extsAddon": undefined,
+  "applyJSLive": false,
+  "applyCSSLive": true,
+  "applyImgLive": true,
+  "exclusions": [".git/", ".svn/", ".hd/", "node_modules/"],
+  "exclusionsAddon": undefined,
+  "originalPath": undefined,
+  "overrideURL": undefined,
+  "usePolling": undefined,
+  "debug": false,
+  "watch": "."
+}
+```
+
+`extsAddons` is to append to the `exts` array. Keep in mind that `["ts", "coffee", "jade", "jsx", "es6", "less", "sass", "scss"]` are usually watched and converted to js or css anyways.
+
+`exclusionsAddon` is to append to the `exclusions` array.
+
+### Sample .livereloadrc
+
+```
+{
+  "watch": ["my-dir/to-watch/", "and/another/dir/to/watch/"],
+  "extAddon": ["ts"]
+}
+```
+
+This file could even be an `.ini` file (refer to `rc`'s documentation).
+
+### Arguments
+
+Thanks to `rc` one can alter any of these via inline arguments:
+
+```
+~ $ cd /path/to-watch
+/path/to-watch $ livereload-simple-server --debug
+```
+
+or
+
+```
+~ $ cd /path/to-watch
+/path/to-watch $ livereload-simple-server --watch=./subdir/to-watch --watch=./another/dir/to-watch --extAddon=ts --extAddons=XYZ
+```
---
+++ bin/livereload-simple-server.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+var livereload = require('livereload')
+var resolve = require('path').resolve
+
+var conf = {
+  port: 35729,
+  exts: ['html', 'css', 'js', 'png', 'gif', 'jpg', 'jpeg', 'php', 'php5', 'py', 'rb', 'erb', 'coffee', 'htm'],
+  extsAddon: undefined, // ['ts', 'coffee', 'jade', 'jsx', 'es6', 'less', 'sass', 'scss'], // these are usually watched and converted to js or css anyways
+  applyJSLive: false,
+  applyCSSLive: true,
+  applyImgLive: true,
+  exclusions: ['.git/', '.svn/', '.hd/', 'node_modules/'],
+  exclusionsAddon: undefined,
+  originalPath: undefined,
+  overrideURL: undefined,
+  usePolling: undefined,
+  debug: false,
+  watch: '.'
+}
+var appCfg = require('rc')('livereload', conf)
+
+appCfg.debug && console.dir(appCfg)
+ 
+var server = livereload.createServer({
+  port: appCfg.port,
+  exts: appCfg.exts.concat(appCfg.extsAddon || []),
+  applyJSLive: appCfg.applyJSLive,
+  applyCSSLive: appCfg.applyCSSLive,
+  applyImgLive: appCfg.applyImgLive,
+  exclusions: appCfg.exclusions.concat(appCfg.exclusionsAddon || []),
+  originalPath: appCfg.originalPath,
+  overrideURL: appCfg.overrideURL,
+  usePolling: appCfg.usePolling,
+  debug: appCfg.debug
+})
+
+appCfg.debug || console.log('LiveReload server started.')
+console.log('Use this snippet in your website:')
+console.log(`<script>document.write('<script src="http://'+(location.host||'localhost').split(':')[0]+':${appCfg.port}/livereload.js?snipver=1"></'+'script>')</script>`)
+
+var paths = appCfg.watch
+if (paths === '.') {
+  paths = resolve('.')
+}
+
+server.watch(paths)
